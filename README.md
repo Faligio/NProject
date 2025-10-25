d0e5d4993cbda28701768c5ce4f3e74a2b885092
Fab <dimartinofabio13@gmail.com>
2025-10-25 11:58:01 +0200

    chore: initial UE5 repo with LFS

---
+++ README.md
@@ -0,0 +1,5 @@
+# NProject
+
+Projet Unreal Engine 5.6
+- Ouvrir avec `NProject.sln` dans Rider.
+- Ne pas versionner Binaries/ Intermediate/ DerivedDataCache/ Saved/.
---
+++ Source/NProject/DummyBootstrap.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class NPROJECT_API DummyBootstrap
+{
+public:
+	DummyBootstrap();
+	~DummyBootstrap();
+};
